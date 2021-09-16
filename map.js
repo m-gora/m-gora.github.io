cfa8509850d9d53687415c31d104e6031aac23b9
Marco <blackanthrax666@googlemail.com>
2021-09-16 23:00:38 +0200

    Update map.js

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   new google.maps.Map(document.getElementById("map"), {
     mapId: "7eb2dc4bf34c42fc",
-    center: { lat: 48.85, lng: 2.35 },
+    center: { lat: 52.52, lng: 13.40 },
     zoom: 12,
   });
 }
