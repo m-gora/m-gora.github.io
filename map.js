82cba651a2e67f8b28330aec81fa5cdcee76783e
Marco <blackanthrax666@googlemail.com>
2021-09-16 22:48:36 +0200

    Create map.js

---
+++ map.js
@@ -0,0 +1,9 @@
+let map;
+
+function initMap() {
+  new google.maps.Map(document.getElementById("map"), {
+    mapId: "7eb2dc4bf34c42fc",
+    center: { lat: 48.85, lng: 2.35 },
+    zoom: 12,
+  });
+}
